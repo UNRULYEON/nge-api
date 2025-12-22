08c546d48ffd7be066daf4ff87d0fbd24367c8fd
Amar Kisoensingh <amar@kisoensingh.sh>
2025-12-22 22:28:03 +0100

    feat: initialise scalar

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(staticPlugin())
+  .use(
+    openapi({
+      documentation: {
+        info: {
+          title: "NGE API",
+          version: "0.0.1",
+        },
+      },
+      path: "/",
+      specPath: "/openapi.json",
+      scalar: {
+        favicon: "public/rei-plush-favicon.svg",
+      },
+      exclude: {
+        paths: ["/", "/public/*"],
+      },
+    }),
+  )
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
